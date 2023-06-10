87664bd2d9b53b92d4a3a0ba6838aeabe34b9a1c
meritissimo1 <marcelomoreira1905@gmail.com>
2023-06-10 18:35:20 -0300

    gdb ctf

---
+++ lab_dojo.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <time.h>
+
+
+char* rand_string() {
+    static const char chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int size = rand() % 10 + 1;  
+    char* string = (char*)malloc((size + 1) * sizeof(char));
+    int i;
+
+    for (i = 0; i < size; i++) {
+        int index = rand() % (sizeof(chars) - 1); 
+        string[i] = chars[index];  
+    }
+
+    string[size] = '\0';  
+
+    return string;
+}
+
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	current;
+
+	current = 0;
+	while ((s1[current] || s2[current]) && current < n)
+	{
+		if (s1[current] != s2[current])
+			return ((unsigned char)s1[current] - (unsigned char)s2[current]);
+		current++;
+	}
+	return (0);
+}
+
+int	main( void )
+{
+	char 	*input;
+	size_t	len;
+
+	char *result = rand_string();
+	input = readline("minihell - $> ");
+	len = strlen(input);
+	while (ft_strncmp(input, result, len))
+	{
+		printf("Fail\n");
+		input = readline("$> ");
+	}
+	printf("Boua!\n");
+
+	free(result);
+}
